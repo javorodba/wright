8448fe66d22b3b5c06b2164a91c7fc9733deee22
Javier Rodas <wm.rodas.javier@gmail.com>
2014-06-06 14:50:30 -0600

    Explanation comments added to the horizontal layout file

--- code/html/mod_articles_categories/horizontal_items.php
+++ code/html/mod_articles_categories/horizontal_items.php
@@ -9,7 +9,7 @@
 
 // no direct access
 defined('_JEXEC') or die;
-$i = 0;
+$i = 0; //start counter
 foreach ($list as $item) :
 ?>
 
@@ -20,23 +20,27 @@ foreach ($list as $item) :
 		{	 		
 			//echo JHtml::_('content.prepare', $item->description, $item->getParams(), 'mod_articles_categories.content');?>
             <?php
-            if ($item->getParams()->get('image'))
+            if ($item->getParams()->get('image')) //start condition if exist image
 			{?>
         	<img src="<?php echo $item->getParams()->get('image');?> ?>" />
             <?php
 			} else {
 			?>
             <div>noimage</div>
-            <?php } ?>
-
+            <?php 
+			} //end condition to see if exist image
+			?>
+			
+            <!--Shows title text and description below the image-->
             <div class="text_frontcat">
                 <span class="title_frontcat"><?php echo $item->title;?></span> - <a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>" class="description_frontcat"><?php echo strip_tags($item->description);?></a>
             </div>
+            <!--End title text and description below the image-->
 		<?php }
 		?>
  </li>
 </div>
 <?php 
-if (++$i == 3) break;
+if (++$i == 3) break; //Condition to only show 3 categories
 endforeach; 
 ?>
