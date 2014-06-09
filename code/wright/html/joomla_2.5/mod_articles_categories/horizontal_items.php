3e00380dfd102c39b0c26db949cc7dfeee074f4c
Javier Rodas <wm.rodas.javier@gmail.com>
2014-06-09 16:19:50 -0600

    joomla 2.5 horizontal items

--- code/wright/html/joomla_2.5/mod_articles_categories/horizontal_items.php
+++ code/wright/html/joomla_2.5/mod_articles_categories/horizontal_items.php
@@ -9,37 +9,38 @@
 
 // no direct access
 defined('_JEXEC') or die;
+$i = 0; //start counter
 foreach ($list as $item) :
-
 ?>
-	<li <?php if ($_SERVER['PHP_SELF'] == JRoute::_(ContentHelperRoute::getCategoryRoute($item->id))) echo ' class="active"';?>> <?php $levelup=$item->level-$startLevel -1; ?>
-  <h<?php echo $params->get('item_heading')+ $levelup; ?>>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>">
-			<i class="icon-folder-open"></i>  <?php // Wright v.3: Added icon ?>
-		<?php echo $item->title;?></a>test
-   </h><?php echo $params->get('item_heading')+ $levelup; ?>>
-   
-<?php if ($this->params->get('show_description_image') && $this->parent->getParams()->get('image')) : ?>
-     <img src="<?php echo $this->parent->getParams()->get('image'); ?>" alt=" "/>
-<?php endif; ?>
 
+<div style="width:31%; padding:0 1%; float:left" class="horizontal_container">
+	<li <?php if ($_SERVER['PHP_SELF'] == JRoute::_(ContentHelperRoute::getCategoryRoute($item->id))) echo ' class="active"';?>> <?php $levelup=$item->level-$startLevel -1; ?>
 		<?php
 		if($params->get('show_description', 0))
-		{
-			echo JHtml::_('content.prepare', $item->description, $item->getParams(), 'mod_articles_categories.content');
-		}
-		
-		
-		if($params->get('show_children', 0) && (($params->get('maxlevel', 0) == 0) || ($params->get('maxlevel') >= ($item->level - $startLevel))) && count($item->getChildren()))
-		{
-
-			echo '<ul class="nav nav-list">';  // Wright v.3: Added nav nav-list classes
-			$temp = $list;
-			$list = $item->getChildren();
-			require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'horizontal').'_items');
-			$list = $temp;
-			echo '</ul>';
-		}
+		{	 		
+			//echo JHtml::_('content.prepare', $item->description, $item->getParams(), 'mod_articles_categories.content');?>
+            <?php
+            if ($item->getParams()->get('image')) //start condition if exist image
+			{?>
+        	<img src="<?php echo $item->getParams()->get('image');?> ?>" />
+            <?php
+			} else {
+			?>
+            <div>noimage</div>
+            <?php 
+			} //end condition to see if exist image
+			?>
+			
+            <!--Shows title text and description below the image-->
+            <div class="text_frontcat">
+                <span class="title_frontcat"><?php echo $item->title;?></span> - <a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>" class="description_frontcat"><?php echo strip_tags($item->description);?></a>
+            </div>
+            <!--End title text and description below the image-->
+		<?php }
 		?>
  </li>
-<?php endforeach; ?>
+</div>
+<?php 
+if (++$i == 3) break; //Condition to only show 3 categories
+endforeach; 
+?>
